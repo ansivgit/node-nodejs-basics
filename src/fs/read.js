262a41aba3fe6706011a5344bdfb1c272acede19
ansivgit <ansiv@yandex.ru>
2025-10-23 18:16:12 +0300

    feat: add list & read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { readFile } from 'fs';
+
+const FILE_PATH = './src/fs/files/fileToRead.txt';
+const ERR_MSG = 'FS operation failed';
+
 const read = async () => {
-  // Write your code here
+  readFile(FILE_PATH, 'utf8', (err, data) => {
+      if (err) {
+        console.error('File does not exist');
+        throw new Error(ERR_MSG);
+      }
+
+      console.info('Content:\n\n', data);
+    });
 };
 
 await read();
