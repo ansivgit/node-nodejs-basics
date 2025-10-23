822379a1089fa763ac6ed90d20a90c7fab06005c
ansivgit <ansiv@yandex.ru>
2025-10-23 17:11:58 +0300

    feat: add rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
+import { access, constants, rename as rn} from 'fs';
+
+const OLD_FILE = './src/fs/files/wrongFilename.txt';
+const NEW_FILE = './src/fs/files/properFilename.md';
+const ERR_MSG = 'FS operation failed';
+
 const rename = async () => {
-  // Write your code here
+  access(NEW_FILE, constants.F_OK, (err) => {
+    if (!err) {
+      console.error('File with the new name exists');
+      throw new Error(ERR_MSG);
+    }
+
+    rn(OLD_FILE, NEW_FILE, (err) => {
+      if (err) {
+        console.error('File does not exist');
+        throw new Error(ERR_MSG);
+      }
+
+      console.log('Rename complete!');
+    });
+  });
 };
 
 await rename();
