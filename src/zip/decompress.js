8c4ce3a74fa452379f71ddafb8d5d50aa72fd85e
ansivgit <ansiv@yandex.ru>
2025-10-24 16:58:51 +0300

    feat: add zip functions

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
+import { createUnzip } from 'zlib';
+
+const SRC_PATH = './src/zip/files/archive.gz';
+const DEST_PATH = './src/zip/files/fileToCompress.txt';
+
 const decompress = async () => {
-  // Write your code here
+  try {
+    await pipeline(
+      createReadStream(SRC_PATH),
+      createUnzip(),
+      createWriteStream(DEST_PATH),
+
+    );
+
+    console.log('Decompression completed');
+  } catch (error) {
+    console.error('Decompression failed');
+    throw error;
+  }
 };
 
 await decompress();
