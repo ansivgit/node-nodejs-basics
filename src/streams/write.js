6530ab3b48649f15a535cc2ac1626358d1b6833a
ansivgit <ansiv@yandex.ru>
2025-10-25 20:42:00 +0300

    feat: add streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
+import { createWriteStream } from 'fs';
+
+const DEST_PATH = 'src/streams/files/fileToWrite.txt';
+
 const write = async () => {
-  // Write your code here
+  const writeStream = createWriteStream(DEST_PATH, 'utf8');
+
+  console.info('Enter your text, than press Enter and Ctrl+D (Linux/macOS) or Ctrl+Z then Enter (Windows)\n');
+
+  process.stdin.pipe(writeStream);
+
+  writeStream.on('finish', () => {
+    console.info('\nText saved to fileToWrite.txt!');
+  });
+
+  writeStream.on('error', (err) => {
+    console.error('Error writing file:', err);
+  });
 };
 
 await write();
